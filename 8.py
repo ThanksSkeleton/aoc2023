d6a40e04cf26226b41cb4aed21096c1e1c361c41
ThanksSkeleton <Thanks.Skeleton@gmail.com>
2023-12-09 15:35:18 -0800

    8 wip

--- 8.py
+++ 8.py
@@ -5,8 +5,9 @@ from pathlib import Path
 import functools
 import os
 
-Node = Tuple[str, str]
-Label_And_Node = Tuple[str, Tuple[str, str]]
+BranchNodeKey = str
+BranchNodeValue = Tuple[str, str]
+BranchNodeDict = Dict[BranchNodeKey, BranchNodeValue]
 
 def lines(full_file_path: str) -> List[str]:
     p = Path(full_file_path)
@@ -20,48 +21,63 @@ def work(lines: List[str]) -> str:
     map = {}
     for ln in label_and_nodes:
         map[ln[0]] = ln[1]
-    return str(navigate_all(map, instructions))
+    return str(follow_all_trackers(map, instructions))
 
-def parse_line(input: str) -> Label_And_Node:
+def parse_line(input: str) -> Tuple[BranchNodeKey, BranchNodeValue]:
     clean_1 = input.replace("(", "")
     clean_2 = clean_1.replace(")", "")
     splitted_1 = clean_2.split("=")
     splitted_2 = splitted_1[1].split(",")
     return (splitted_1[0].strip(), (splitted_2[0].strip(), splitted_2[1].strip()))
 
-MegacycleNode = Tuple[str, str]
-LinearNode = Tuple[str, int]
-LinearNodeZero = Tuple[str, int, Optional[int]]
+LinearNodeKey = Tuple[str, int]
 
+class ChainTracker:
+    def __init__(self, starter: str):
+        print(f"tracker for {starter}")
+        self.starter = starter
+        self.sequence_array : dict[LinearNodeKey, int] = {}
+        self.reverse_sequence_array : dict[int, LinearNodeKey] = {}
+        self.next_index = 0
+        self.terminus = None
+        self.add((starter, 0))
+
+    def add(self, key: LinearNodeKey):
+        self.sequence_array[key] = self.next_index
+        self.reverse_sequence_array[self.next_index] = key
+        self.next_index = self.next_index+1
+
+    def has_key(self, key: LinearNodeKey) -> bool:
+        return key in self.sequence_array.keys()
+    
+    def follow_all(self, map: BranchNodeDict, sequence: str):
+        print(f"tracking {self.starter}")
+        while True:            
+            current = self.reverse_sequence_array[self.next_index - 1]
+            next_sequence_index = self.next_index % len(sequence)
+            next_direction_string = sequence[next_sequence_index]
+            next_direction = 0 if next_direction_string == "L" else 1
+            next_label = map[current[0]][next_direction]
+            next_node = (next_label, next_sequence_index)
+            if self.has_key(next_node):
+                print(f"for tracker starting at {self.starter} - loop detected at {next_node} {next_sequence_index} after {self.next_index}")
+                return
+            else:
+                self.add(next_node)
+            
 # This can be expressed as
 # n Full Cycles + 1 subcycle of size k
 # k such that some number of starting nodes all align to a zero
 
 
-def build_maps(map: Dict[str, Node], instructions: str) -> Tuple[Dict[str, MegacycleNode], Dict[str, LinearNodeZero]]:
-    for char in instructions:
-
-
-
-def navigate_all(map: Dict[str, Node], instructions: str) -> int:
-    START = "AAA"
-    END = "ZZZ"
-    move_count = 0
-    current_instructions = instructions
-    current_location = START
-    while True:
-        current_move = current_instructions[0]
-        if current_move == "L":
-            current_location = map[current_location][0]
-        else:
-            current_location = map[current_location][1]
-        move_count = move_count + 1
-        if current_location == END:
-            return move_count
-        if len(current_instructions) == 1:
-            current_instructions = instructions
-        else: 
-            current_instructions = current_instructions[1:]
+def follow_all_trackers(map: BranchNodeDict, instructions: str):
+    trackers = []
+    for key in map.keys():
+        if key.endswith("A"):
+            trackers.append(ChainTracker(key))
+    
+    for t in trackers:
+        t.follow_all(map, instructions)
 
 #path = os.path.join("data_files", "8_1_example_1.txt")
 #path = os.path.join("data_files", "8_1_example_2.txt")
